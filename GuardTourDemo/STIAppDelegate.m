ce29c9cf5d08c59138e2cf706f4235a79a358c73
Lee Sitek <lsitek@seetechnologies.ca>
2014-10-15 23:39:36 -0600

    Removed no longer used Roximity SDK code from app delegate

--- GuardTourDemo/STIAppDelegate.m
+++ GuardTourDemo/STIAppDelegate.m
@@ -7,7 +7,6 @@
 //
 
 #import "STIAppDelegate.h"
-#import "ROXIMITYlib/ROXIMITYlib.h"
 #import "STIBeacon.h"
 #import "STIBeaconController.h"
 
@@ -22,13 +21,13 @@
     NSUUID *beaconRoximityUUID = [[NSUUID alloc] initWithUUIDString:@"8DEEFBB9-F738-4297-8040-96668BB44281"];
     CLBeaconRegion *beaconRoximityRegion = [[CLBeaconRegion alloc] initWithProximityUUID:beaconRoximityUUID identifier:@"beaconRoximity"];
     
-    [self.locationManager startMonitoringForRegion:beaconRoximityRegion];
+//    [self.locationManager startMonitoringForRegion:beaconRoximityRegion];
     [self.locationManager startRangingBeaconsInRegion:beaconRoximityRegion];
 
     NSUUID *beaconAppleUUID = [[NSUUID alloc] initWithUUIDString:@"E2C56DB5-DFFB-48D2-B060-D0F5A71096E0"];
     CLBeaconRegion *beaconAppleRegion = [[CLBeaconRegion alloc] initWithProximityUUID:beaconAppleUUID identifier:@"beaconApple"];
     
-    [self.locationManager startMonitoringForRegion:beaconAppleRegion];
+//    [self.locationManager startMonitoringForRegion:beaconAppleRegion];
     [self.locationManager startRangingBeaconsInRegion:beaconAppleRegion];
     
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
@@ -107,55 +106,27 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    [ROXIMITYEngine resignActive];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    [ROXIMITYEngine background];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    [ROXIMITYEngine foreground];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    [ROXIMITYEngine active];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    [ROXIMITYEngine removeAlias];
-    [ROXIMITYEngine terminate];
 }
-
-//-(void) application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)error
-//{
-//    [ROXIMITYEngine didFailToRegisterForRemoteNotifications:error];
-//}
-//
-//-(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
-//{
-//    [ROXIMITYEngine didRegisterForRemoteNotifications:deviceToken];
-//}
-//
-//-(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler{
-//    [ROXIMITYEngine didReceiveRemoteNotification:application userInfo:userInfo fetchCompletionHandler:completionHandler];
-//}
-//
-//-(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo{
-//    [ROXIMITYEngine didReceiveRemoteNotification:application userInfo:userInfo];
-//}
-//
-//-(void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification{
-//    [ROXIMITYEngine didReceiveLocalNotification:application notification:notification];
-//}
 
 @end
